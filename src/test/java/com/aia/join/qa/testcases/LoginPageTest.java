77141feaea55424791072716b653e7c286701d13
chowdarykmkm <kakarlamuralee@gmail.com>
2020-09-26 18:22:56 +0530

    Update LoginPageTest.java

--- src/test/java/com/aia/join/qa/testcases/LoginPageTest.java
+++ src/test/java/com/aia/join/qa/testcases/LoginPageTest.java
@@ -6,7 +6,6 @@ import org.testng.annotations.Test;
 import com.aia.join.aia.base.TestBase;
 import com.aia.join.qa.pages.LoginPage;
 
-
 public class LoginPageTest extends TestBase{
 	
 	LoginPage loginPage;
